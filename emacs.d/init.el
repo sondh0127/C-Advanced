fed52ca2b44adced4d0cef5f667d076bb2434206
Hong Son Do <sonstephendo@gmail.com>
2017-09-07 05:51:07 -0700

    init.el

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -12,3 +12,21 @@
 (require 'diminish)
 (require 'bind-key)
 (org-babel-load-file (expand-file-name "~/.emacs.d/myinit.org"))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(cua-mode t nil (cua-base))
+ '(display-time-mode t)
+ '(package-selected-packages
+   (quote
+	(multiple-cursors counsel expand-region hungry-delete beacon tabbar smartparens undo-tree powerline alect-themes eziam-theme moe-theme base16-theme color-theme org-bullets try yasnippet flycheck auto-complete which-key ggtags use-package)))
+ '(show-paren-mode t)
+ '(tool-bar-mode nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:family "Source Code Pro" :foundry "ADBO" :slant normal :weight normal :height 120 :width normal)))))
