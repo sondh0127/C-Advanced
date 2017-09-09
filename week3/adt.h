d0bf9fb44b9317a597a836a755d4c8c62e6395b3
Hong Son Do <sonstephendo@gmail.com>
2017-09-09 16:46:26 +0700

    emacs

--- week3/adt.h
+++ week3/adt.h
@@ -1,12 +1,5 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <ctype.h>
-#include <time.h>
-
-#define INITIAL_SIZE 100
-#define INCREMENTAL_SIZE 10
-
+#ifndef ADT_H
+#define ADT_H
 typedef struct {
 	//Ten - sdt
 	char name[80];
@@ -21,29 +14,20 @@ typedef struct {
 	int size;
 }PhoneBook;
 
-PhoneBook createPhoneBook()
-{// Create new Phone Book
-	PhoneBook p;
-	p.entries = (PhoneEntry*)malloc(sizeof(PhoneEntry));
-	p.total = 0;
-	p.size = 0;
-	return p;
-}
 
-void dropPhoneBook(PhoneBook* book)
-{// remove all Phone Book
-	free(book->entries);
-	book->size = 0;
-}
+PhoneBook createPhoneBook();
+void dropPhoneBook(PhoneBook* book);
 
-void addPhoneNumber(char* name, long number, PhoneBook* book)
-{// add new PhoneNumber-Person(overwrite)
-	
-}
-PhoneEntry *getPhoneNumber(char* name, PhoneBook book)
-{// find an entity from Phone Book return NULL if this entry can not find.
-	return NULL;
-}
+int binarySearch(PhoneEntry* entries, int l, int r, char* name, int* found);
+void addPhoneNumber(char* name, long number, PhoneBook* book);
+PhoneEntry *getPhoneNumber(char* name, PhoneBook book);
+
+
+
+#endif /* ADT_H */
+
+
+
 
 
 
--- week3/main.c
+++ week3/main.c
@@ -1,8 +1,29 @@
 // The rule: type everything from slide first
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <time.h>
 #include "adt.h"
-#include ""
+
 int main()
 {
-	
+	PhoneBook book;
+//Khoi tao & chen
+	book = createPhoneBook();
+	addPhoneNumber("Do Lam", 909090, &book);
+	addPhoneNumber("Tuan Dung", 929292, &book);
+	addPhoneNumber("Anh Thang", 919191, &book);
+	addPhoneNumber("Ngo Phong", 949494, &book);
+//Hien thi danh sach book
+	for (int i = 0; i < book.total + 1; i++) {
+		printf("%d|%s\t|%ld\n", i+1, book.entries[i].name,  book.entries[i].number);
+	}
+//Tim kiem
+	PhoneEntry* entry = getPhoneNumber("Do Lam", book);
+	if(entry == NULL)
+		printf("Can not found!\n");
+	else
+		printf("%ld\n", entry->number);
 	return 0;
 }
